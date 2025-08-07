21f0b8f2ce6a3f2438eacc31b7c19cee9d9942fd
GodJinSoul <joshgabriela@gmail.com>
2025-08-07 14:33:14 +0800

    test

---
+++ TestRun/Student/Anda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRun.Student
+{
+    internal class Anda : Student
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Anda is running the test.");
+        }
+    }
+}
